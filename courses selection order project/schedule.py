8983085eb556738bd1aad0cdd3653fdf968be1c0
siahuat0727 <tansiahuat@gmail.com>
2017-11-16 14:56:19 +0800

    report ing

--- courses selection order project/schedule.py
+++ courses selection order project/schedule.py
@@ -23,7 +23,7 @@ course,day,start,end,happiness,front=multidict({
 		('JapaneseA'):[1,7,8,8,'A1'],
 		('JapaneseB'):[2,3,4,6,'A1'],
 		('JapaneseC'):[2,7,8,4,'A1'],
-		('GeneralEducationA'):[1,7,8,20,'A9'],
+		('GeneralEducationA'):[1,7,8,10,'A9'],
 		('GeneralEducationB'):[2,7,8,5,'A9'],
 		('GeneralEducationC'):[5,7,8,2,'A9'],
 		})
@@ -42,12 +42,10 @@ A9 = [c for c in course if front[c] == 'A9']
 RE = [c for c in course if front[c] != 'A1' and front[c] != 'A9']
 collide = [[],[]]
 for cA1 in A1:
-	collideA9 = [cA9 for cA9 in A9 if (start[cA1] > end[cA9] or end[cA1] < start[cA9]) == False] # TODO check youXianQuan
+	collideA9 = [cA9 for cA9 in A9 if (start[cA1] > end[cA9] or end[cA1] < start[cA9]) == False]
 	if collideA9: # if len(collideA9) > 0
 		collide[0].append(cA1)
 		collide[1].append(collideA9)
-print("colide:")
-print(collide)
 
 m = Model("course_schedule")
 
@@ -55,15 +53,12 @@ choose_RE = m.addVars(RE, vtype=GRB.BINARY, name="choose_RE")
 choose_A1 = m.addVars(A1, len(A1), vtype=GRB.BINARY, name="choose_A1")
 choose_A9 = m.addVars(A9, len(A9), vtype=GRB.BINARY, name="choose_A9")
 
-choose_collide = m.addVars(A1, len(A1), A9, len(A9), vtype=GRB.CONTINUOUS, name="choose_collide")
-over_add  = m.addVar(vtype=GRB.CONTINUOUS, name="over_add")
-
 m.update()
 
 p_A1 = 0.1
 prob_A1 = [p_A1 * ((1-p_A1)**i) for i in range(len(A1))]
 
-p_A9 = 0.5
+p_A9 = 0.24
 prob_A9 = [p_A9 * ((1-p_A9)**i) for i in range(len(A9))]
 
 m.setObjective(
@@ -75,44 +70,24 @@ m.setObjective(
 	   	+ quicksum(prob_A9[i] * choose_A9[c, i] * happiness[c] for c in A9 for i in range(len(A9)) if all(start[c] > end[cc] or end[c] < start[cc] for cc in [c for c in RE if choose_RE[c] == 1]))
 		# subtract the ideal value that added twice(A1 collide with A9)
 		- quicksum(prob_A1[i] * choose_A1[cA1, i] * prob_A9[j] * choose_A9[cA9, j] * min(happiness[cA9], happiness[cA1]) for cA1, cA9s in zip(collide[0], collide[1]) for cA9 in cA9s for i in range(len(A1)) for j in range(len(A9)))
-		# - over_add
 		,GRB.MAXIMIZE)
 
+# required courses must be selected
 m.addConstrs(choose_RE[c] == 1 for c in RE if front[c] == 'R')
 
 # avoid collision on required and elective courses
 m.addConstrs(quicksum(choose_RE[c] for c in RE if start[c] <= t <= end[c]) <= 1 for t in range(total_t)) # avoid more than one courses at [t, t+1)
-# TODO bixiu zhuang xuanxiu 
 
+# assignment problem
 # 序序有課填
 m.addConstrs(choose_A1.sum(c, '*') == 1 for c in A1)
 # 課課有序選
 m.addConstrs(choose_A1.sum('*', i) == 1 for i in range(len(A1)))
-		
 # 序序有課填
 m.addConstrs(choose_A9.sum(c, '*') == 1 for c in A9)
 # 課課有序選
 m.addConstrs(choose_A9.sum('*', i) == 1 for i in range(len(A9)))
 		
-# m.addConstr(over_add == quicksum(prob_A1[i] * choose_A1[cA1, i] * prob_A9[j] * choose_A9[cA9, j] * min_(happiness[cA9], happiness[cA1]) for cA1 in collide[0] for cA9s in collide[1] for cA9 in cA9s for i in range(len(A1)) for j in range(len(A9))))
-m.addConstr(over_add == quicksum(prob_A1[i] * choose_A1[cA1, i] * prob_A9[j] * choose_A9[cA9, j] * min(happiness[cA9], happiness[cA1]) for cA1 , cA9s in zip(collide[0], collide[1]) for cA9 in cA9s for i in range(len(A1)) for j in range(len(A9))) , name="c_over_add")
-
-'''
-# m.addGenConstrsMin(over_add, [choose_RE['compiler'], choose_RE['programming_language']], name="bla")
-m.addConstr(choose_collide['JapaneseA', 1, 'GeneralEducationA', 1] == prob_A1[1]*choose_A1['JapaneseA', 1]*prob_A9[1]*choose_A9['GeneralEducationA', 1]*min(happiness['JapaneseA'], happiness['GeneralEducationA']))
-m.addConstrs(choose_collide[cA1][i][cA9][j] == 
-		prob_A1[i] * choose_A1[cA1, i] *
-	   	prob_A9[j] * choose_A9[cA9, j] *
-	   	min(happiness[cA9], happiness[cA1]) 
-		for cA1 in collide[0] 
-		for cA9s in collide[1] 
-		for cA9 in cA9s 
-		for i in range(len(A1)) 
-		for j in range(len(A9)))
-'''
-
-# m.addConstrs(over_add == min_(choose_RE['compiler'], choose_RE['programming_language']))
-
 m.optimize()
 
 m.write("course_schedule.lp")
